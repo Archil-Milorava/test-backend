8e85c1b15437d28a0629469cef1b84bfd84ba64f
Archil-Milorava <achimilorava16@gmail.com>
2025-03-02 22:58:13 +0400

    sadasd

--- server/src/server.js
+++ server/src/server.js
@@ -21,6 +21,12 @@ const server = createServer(app);
 //   });
 // }
 
+app.get("*", (req,res) => {
+  res.status(200).json({
+    message: "hello from backend"
+  })
+})
+
 server.listen(PORT, async () => {
   await connectDB();
   console.log(`Server is running on port ${PORT}`);
